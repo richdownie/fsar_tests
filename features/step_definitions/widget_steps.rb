b5dc165736a4c18cf7d2bfd1378f61f600e4f3ce
Rich Downie <rich@richdownie.com>
2016-03-26 12:16:24 -0400

    adding rspec

--- features/step_definitions/widget_steps.rb
+++ features/step_definitions/widget_steps.rb
@@ -5,3 +5,7 @@ end
 Then /I verify widgets display on the page/ do
   @browser.find_element(:id, 'widgets').displayed?
 end
+
+Then /I verify the widgets table has "(.*)" rows/ do |row_count|
+  puts @browser.find_elements(css: '#widgets_table tr').count - 1
+end 
--- features/widgets.feature
+++ features/widgets.feature
@@ -3,3 +3,4 @@ Feature: FSAR Widget Tests
 Scenario: Verify widgets display on the page
   Given I am on the fsar widgets page
   Then I verify widgets display on the page
+  Then I verify the widgets table has "3" rows
